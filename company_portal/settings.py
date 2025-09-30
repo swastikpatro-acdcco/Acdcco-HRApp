1133b7eaa6255e9ce88eb033193865d354d60dee
aman3singh <aman3singh.work@gmail.com>
2025-09-30 17:25:13 -0400

    Updated DB connection to DB hosted on Supabase

--- company_portal/settings.py
+++ company_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,6 +93,7 @@ WSGI_APPLICATION = 'company_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
@@ -99,12 +101,26 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'acdc_hrapp',
         'USER': 'postgres',
-        'PASSWORD': 'your_password',  # Replace with your real password
+        'PASSWORD': '',  # Replace with your real password
         'HOST': 'localhost',  # or '127.0.0.1'
         'PORT': '5432',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='postgres'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS': {
+            'sslmode': 'require',  # Supabase requires SSL
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
